ce82cdb3a9ca17c811e217db91a48c883104f016
drawfig <usmckdm@gmail.com>
2025-09-21 22:28:58 -0700

    added a command history and then the removal of the .git directory on initialization of a new server

--- Emberwhisk/src/Agents/default_agent.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Agents;
-
-class default_agent {
-    public function bounce_txt() {
-        return "Boing!\n";
-    }
-}
--- Emberwhisk/src/routes/Request_Routes.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class Request_Routes {
-    public $REQUEST_ROUTES = [
-        "bounce" => ["class" => "default_handler", "method" => "bounce", "protected" => false],
-    ];
-}
